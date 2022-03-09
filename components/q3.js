aa93c1cb7c8404019d33612ced8d563301b76dc9
Tina Gao <thgao@uwaterloo.ca>
2022-03-08 22:23:57 -0500

    Clean up layout

--- components/q3.js
+++ components/q3.js
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Q1() {
+export default function Q3() {
   return (
     <div >
-      <h1>Hi! I'm a Question and I have lots of thoughts</h1>
+      <h1>This a third question!! </h1>
     </div>
   )
 }
