2b760b8487608e06d97ce2ff1252398b6fd84a44
Tina Gao <thgao@uwaterloo.ca>
2022-03-19 12:54:17 -0400

    Create counter components with sum checking

--- components/q2.jsx
+++ components/q2.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 // import styles from '../styles/Home.module.css';
+import { TotalSumForm, CounterButton } from './form-input/TotalSumForm';
 
 export default function Q2() {
   return (
     <div>
       <h1>Hi! I'm a DIFFERENT Question :) </h1>
+      <TotalSumForm
+        title="Here is custom form title like some instructions"
+        total={10}
+      >
+        <CounterButton
+          id="q2-1"
+          label="1Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+        <CounterButton
+          id="q2-2"
+          label="2Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+        <CounterButton
+          id="q2-3"
+          label="3Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+        <CounterButton
+          id="q2-4"
+          label="4Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+        <CounterButton
+          id="q2-5"
+          label="5Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+        <CounterButton
+          id="q2-6"
+          label="6Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+        <CounterButton
+          id="q2-7"
+          label="7Spending money or time is hard! Spending money or time is hard! Spending money or time is hard!"
+        />
+      </TotalSumForm>
     </div>
   );
 }
