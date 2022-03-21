fc72f8405be95183faed554a7d909370e74fd478
Tina Gao <thgao@uwaterloo.ca>
2022-03-20 20:32:04 -0400

    Fix indenting

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
-AIRTABLE_BASE_ID:process.env.AIRTABLE_BASE_ID,
-AIRTABLE_TABLE_NAME:process.env.AIRTABLE_TABLE_NAME
+    AIRTABLE_BASE_ID:process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME:process.env.AIRTABLE_TABLE_NAME
   }
 }
 
